436d5772a2cd5adb4d4917b4775bb3a5c4534d65
Timofey Khirianov <tkhirianov@gmail.com>
2015-10-04 14:26:39 +0300

    Удалил тесты

--- content/extra/lab6/problems/P/tests/gen.py
+++
@@ -1,5 +0,0 @@
-from random import *
-
-for i in xrange(1000):
-	print int(random()*1000),
-print 0
--- content/extra/lab6/problems/S/tests/solution.py
+++
@@ -1,7 +0,0 @@
-def move(n, a, b):
-    if n > 0:
-        move(n - 1, a, 6 - a - b)
-        print(n, a, b)
-        move(n - 1, 6 - a - b, b)
-
-move(int(input()), 1, 3)
