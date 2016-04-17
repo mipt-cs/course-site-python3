3b1fbc0647634726104484a0a882bb0f95e60797
Alexey Ermakov <fufler@gmail.com>
2016-04-17 22:56:44 +0300

    Тесты на findall

--- content/solutions/lab25/simple_findall.py
+++ content/solutions/lab25/simple_findall.py
@@ -7,3 +7,43 @@
 # 1 a 1 2 b ---> a, b
 # z 2 y     ---> z, y
 REGEXP_1 = '[a-z]+'
+
+# aaa bbb ccc ---> aaa, bbb, ccc
+# ddd eee fgh ---> ddd, eee, fgh
+# a1b c2d e3f ---> a1b, c2d, e3f
+REGEXP_2 = r'\w+'
+
+# a aa aaa ---> aa, aaa
+# b bb bbb ---> bb, bbb
+# a bb aaa ---> bb, aaa
+REGEXP_3 = '[ab]{2,3}'
+
+# 1.1.1.1 aaaa bbbbb      ---> 1.1.1.1
+# a.a.a.a bbbb 2.2.2.2    ---> 2.2.2.2
+# 3.3.3.3 cccc 4.4.4.4    ---> 3.3.3.3, 4.4.4.4
+# 255.23.0.1 cccc 4.4.4.4 ---> 255.23.0.1, 4.4.4.4
+# 255.0.23.1 cccc 4.4.4.4 ---> 255.0.23.1, 4.4.4.4
+REGEXP_4 = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+
+# aaa Abbb ccc ---> Abbb
+# Aaa Abbb ccc ---> Aaa, Abbb
+# Caa Cbb Accc ---> Accc
+REGEXP_5 = r'\bA\w+'
+
+# a b c d e f ---> a, b, e, f
+# abcdef      ---> a, b, e, f
+# adf         ---> a, f
+# acf         ---> a, f
+REGEXP_6 = r'[^cd ]'
+
+# aaa +1.0 bb              ---> +1.0
+# aaa -1.0 bb              ---> -1.0
+# aaa -123.234 bb +111.999 ---> -123.234, +111.999
+REGEXP_7 = r'[+-]\d+.\d+'
+
+# aaa 18-04-2016 bbb            ---> 18-04-2016
+# aaa 18.04.2016 bbb            ---> 18.04.2016
+# aaa 18-04-ABCD bbb 18.04.2016 ---> 18.04.2016
+# aaa 18/04/ABCD bbb 18/04/2016 ---> 18/04/2016
+# aaa 18/04/ABCD bbb 18/4/2016  ---> 18/4/2016
+REGEXP_8 = r'\d{1,2}[-/\.]\d{1,2}[-/\.]\d{4}'
--- content/solutions/lab25/simple_findall_test.py
+++ content/solutions/lab25/simple_findall_test.py
@@ -14,6 +14,67 @@ class SimpleFindallTest(unittest.TestCase):
                 '1 a 1 2 b': ['a', 'b'],
                 'z 2 y': ['z', 'y']
             }
+        ),
+        'REGEXP_2': (
+            FINDALL,
+            {
+                'aaa bbb ccc': ['aaa', 'bbb', 'ccc'],
+                'ddd eee fgh': ['ddd', 'eee', 'fgh'],
+                'a1b c2d e3f': ['a1b', 'c2d', 'e3f']
+            }
+        ),
+        'REGEXP_3': (
+            FINDALL,
+            {
+                'a aa aaa':  ['aa', 'aaa'],
+                'b bb bbb':  ['bb', 'bbb'],
+                'a bb aaa':  ['bb', 'aaa']
+            }
+        ),
+        'REGEXP_4': (
+            FINDALL,
+            {
+                '1.1.1.1 aaaa bbbbb': ['1.1.1.1'],
+                'a.a.a.a bbbb 2.2.2.2': ['2.2.2.2'],
+                '3.3.3.3 cccc 4.4.4.4': ['3.3.3.3', '4.4.4.4'],
+                '255.23.0.1 cccc 4.4.4.4': ['255.23.0.1', '4.4.4.4'],
+                '255.0.23.1 cccc 4.4.4.4': ['255.0.23.1', '4.4.4.4']
+            }
+        ),
+        'REGEXP_5': (
+            FINDALL,
+            {
+                'aaa Abbb ccc': ['Abbb'],
+                'Aaa Abbb ccc': ['Aaa', 'Abbb'],
+                'Caa Cbb Accc': ['Accc']
+            }
+        ),
+        'REGEXP_6': (
+            FINDALL,
+            {
+                'a b c d e f': ['a', 'b', 'e', 'f'],
+                'abcdef': ['a', 'b', 'e', 'f'],
+                'adf': ['a', 'f'],
+                'acf': ['a', 'f']
+            }
+        ),
+        'REGEXP_7': (
+            FINDALL,
+            {
+                'aaa +1.0 bb': ['+1.0'],
+                'aaa -1.0 bb': ['-1.0'],
+                'aaa -123.234 bb +111.999': ['-123.234', '+111.999']
+            }
+        ),
+        'REGEXP_8': (
+            FINDALL,
+            {
+                'aaa 18-04-2016 bbb': ['18-04-2016'],
+                'aaa 18.04.2016 bbb': ['18.04.2016'],
+                'aaa 18-04-ABCD bbb 18.04.2016': ['18.04.2016'],
+                'aaa 18/04/ABCD bbb 18/04/2016': ['18/04/2016'],
+                'aaa 18/04/ABCD bbb 18/4/2016 ': ['18/4/2016']
+            }
         )
     }    
   
