e0c1e8f3827c30b0314320c6d039957a42b8f543
Timofey Khirianov <tkhirianov@gmail.com>
2017-09-01 12:13:09 +0300

    Начальное состояние репозитория к 2017-2018 учебному году

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://judge.mipt.ru/mipt_cs_on_python3_2016'
+SITEURL = 'http://judge.mipt.ru/mipt_cs_on_python3'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
