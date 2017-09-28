acf970f0a92671bd8b2b501321cc609a4765e6e1
Ilya Kompaniets <jenkslex@gmial.com>
2017-09-28 18:39:26 +0300

    Added examples for lab 5

---
+++ content/code/lab5/2.py
@@ -0,0 +1,49 @@
+import graphics as gr
+
+SIZE_X = 400
+SIZE_Y = 400
+
+window = gr.GraphWin("Model", SIZE_X, SIZE_Y)
+
+coords = gr.Point(200, 200)
+
+velocity = gr.Point(1, -2)
+
+
+def add(point_1, point_2):
+    new_point = gr.Point(point_1.x + point_2.x,
+                         point_1.y + point_2.y)
+
+    return new_point
+
+
+def clear_window():
+    rectangle = gr.Rectangle(gr.Point(0, 0), gr.Point(SIZE_X, SIZE_Y))
+    rectangle.setFill('green')
+    rectangle.draw(window)
+
+
+def draw_ball(coords):
+    circle = gr.Circle(coords, 10)
+    circle.setFill('red')
+
+    circle.draw(window)
+
+
+def check_coords(coords, velocity):
+    if coords.x < 0 or coords.x > SIZE_X:
+        velocity.x = -velocity.x
+
+    if coords.y < 0 or coords.y > SIZE_Y:
+        velocity.y = -velocity.y
+
+
+while True:
+    clear_window()
+    draw_ball(coords)
+    coords = add(coords, velocity)
+
+    check_coords(coords, velocity)
+
+    gr.time.sleep(0.02)
+
